4aaab8229ac3e7a289bc9d08bbd7110097cf0b9b
jorge <jorgelopeslopes@outlook.com>
2022-05-18 14:58:52 -0300

    criação do menubox

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import styles from "./App.module.scss"
 import { LoginBox } from './components/LoginBox'
+import { MenuBox } from './components/MenuBox'
+
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
 
-   <LoginBox/>
+<MenuBox/>
+ 
    
     )
 }
---
+++ src/components/MenuBox/index.tsx
@@ -0,0 +1,35 @@
+import styles from './styles.module.scss'
+
+export function MenuBox(){
+    return(
+        <div className={styles.MenuBox}>
+           
+           <ul>
+                <li>
+                    <a href="">Motorista</a>
+                </li>
+
+                <li>
+                     <a href="">Veículo</a>
+                </li>
+
+                <li>
+                     <a href="">Agricultor</a>
+                </li>
+
+                <li>
+                     <a href="">Propriedade Rural</a>
+                </li>
+
+                <li>
+                     <a href="">Colheitas</a>
+                </li>
+
+                <li>
+                     <a href="">logout</a>
+                </li>
+           </ul>
+            
+        </div>
+    )
+}
