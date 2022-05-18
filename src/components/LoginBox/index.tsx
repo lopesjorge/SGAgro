d57f389de0bedb32e3044925203faa562529794f
jorge <jorgelopeslopes@outlook.com>
2022-05-18 12:36:34 -0300

    Configuração tela-login

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -3,30 +3,35 @@ import styles from "./styles.module.scss"
 export function LoginBox(){
     return(
     
-        <div className={styles.box_esquerdo}>
-            <div className={styles.font_esquerdo}>
+        <div className={styles.LoginBox}>
+
+            <div className={styles.box_esquerdo}>
+            
+            <p>
             <h1>Bem-vindo</h1>
             <h4>ao Sistema de </h4>
             <h4>Gerenciamento</h4>
             <h4>de Agricultores</h4>
-            </div>
+            </p>
 
-            <div className={styles.login}>
+            </div>
+            
+            <div className={styles.login_style}>
                  
-                 <form action="">
+                 <form action="" className={styles.formulario}>
                     
-                    <input type="email" name="email" id="email" />
-
-                    <input type="password" name="password" id="password" />
-
-                    <a href="">Esqueceu Senha?</a>
-
-                    <a href="">Login</a>
-
-                 </form>
-
+                    <input type="email" name="email" id="email" placeholder="Email" required/>
+                     <br />
+                    <input type="password" name="password" id="password"  placeholder="Password" required/>
+                
+                </form>
+                 
+                 <a href="" className={styles.esqueceuSenha}>Esqueceu Senha?</a>
+                 <br />
+                 <a href="" className={styles.btn_login}>Login</a>
+               
             </div>
-
+            
         </div>
 
     )
