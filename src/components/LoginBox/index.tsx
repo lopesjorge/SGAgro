f3f02b0f38599bcc07e3575b91c11d682529e460
jorge <jorgelopeslopes@outlook.com>
2022-05-21 22:23:27 -0300

    Mudança no design do login

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,37 +1,48 @@
-import styles from "./styles.module.scss"
+import { useState } from 'react'
+import styles from './styles.module.scss'
 
-export function LoginBox(){
+export function  LoginBox(){
+    const [email, setEmail] = useState ("")
+    const [password,setPassword] = useState ("")
+
     return(
-    
-        <div className={styles.LoginBox}>
+          <div className={styles.container}>
+                <div className={styles.container_login}>
+                     <div className={styles.wrap_login}>
+                           <form className={styles.login_form}>
+                               
+                               <span className={styles.title_login_img}>
+                                   <img src="/src/image/SGAgro- logo-L.svg" alt="image-logo" />
+                               </span>
 
-            <div className={styles.box_esquerdo}>
-            
-            <p>
-            <h1>Bem-vindo</h1>
-            <h4>ao Sistema de </h4>
-            <h4>Gerenciamento</h4>
-            <h4>de Agricultores</h4>
-            </p>
+                               <span className={styles.title_login}>
+                                   Login
+                               </span>
 
-            </div>
-            
-            <div className={styles.login_style}>
-                 
-                 <form action="" className={styles.formulario}>
-                    
-                    <input type="email" name="email" id="email" placeholder="Email" required/>
-                     <br />
-                    <input type="password" name="password" id="password"  placeholder="Password" required/>
-                
-                </form>
-                 
-                 <a href="" className={styles.esqueceuSenha}>Esqueceu Senha?</a>
-                 <a type="button" href="" className={styles.btn_login}>Login</a>
-               
-            </div>
-            
-        </div>
+                               <div className={styles.wrap_input}>
+                                   <input className={ email !== "" ? styles.has_value : styles.input } type="email" value={email}
+                                    onChange={(e) => setEmail(e.target.value)}/>
+                                   <span className={styles.focus_input} data-placeholder= 'Email'></span>
+                               </div>
 
+                               <div className={styles.wrap_input}>
+                                   <input className={password!== "" ? styles.has_value : styles.input} type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}/>
+                                   <span className={styles.focus_input} data-placeholder= 'Password'></span>
+                               </div>
+
+                               <div className={styles.container_login_form_btn}>
+                                    <button className={styles.login_form_btn}>Entrar</button>
+                               </div>
+
+                               <div className={styles.esqueceu_senha_cadastro}>
+                                     <a href='#' className={styles.texto_1}>Esqueceu senha?</a>
+                               </div>
+
+                           </form>
+                     </div>
+                </div>
+          </div>
     )
 }
--- src/components/MenuBox/index.tsx
+++ src/components/MenuBox/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss'
 export function MenuBox(){
     return(
         <div className={styles.MenuBox}>
-           
+
            <ul>
                 <li>
                     <a href="">Motorista</a>
